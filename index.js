80138e43c841f76f2f71989efe248a45c408ec99
Tristan Guest <tristanguest@gmail.com>
2020-06-18 11:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var katzDeliLine = [];
 
 function takeANumber(line, name) {
-  return `Welcome, ${name}. You are number ${line.slice(-1)} in line.`
+  return `Welcome, ${name}. You are number ${line.slice(-1) + 1} in line.`
 }
 
 console.log(takeANumber([0, 1, 2, 3], 'Ada'))
