bcbca433f77fdd55bce80f0473a922784f8d7ccb
Tristan Guest <tristanguest@gmail.com>
2020-06-18 11:15:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${parseInt(line.slice(-1)) + 1} in line.`
 }
 
-console.log(takeANumber([0, 1, 2, 3], 'Ada'))
+// console.log(takeANumber([0, 1, 2, 3], 'Ada'))
+
+
+function nowServing(line) {
+  
+  return line.slice(1)
+}
